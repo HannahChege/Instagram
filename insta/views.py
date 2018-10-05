077b772fccc060359db99e4b83c442ea7b3b1b17
HannahChege <chegehannah45@gmail.com>
2018-10-05 22:23:20 +0300

    Added the imports of date,models and django contib.auth

--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,8 @@
 # from django.shortcuts import render
 # from django.http  import HttpResponse
-# import datetime as dt
-# from .models import Image
-# from django.contrib.auth.decorators import login_required
+import datetime as dt
+from .models import Image
+from django.contrib.auth.decorators import login_required
 
 # # Create your views here.
 # @login_required(login_url='/accounts/login/')
