33d7fc062a1aa2c6da5e9e030580c4fce41916af
HannahChege <chegehannah45@gmail.com>
2018-10-05 22:21:35 +0300

    Added the urls.py

--- insta/models.py
+++ insta/models.py
@@ -1,54 +1,54 @@
-from django.db import models
-import datetime as dt
-from django.contrib.auth.models import User
-from tinymce.models import HTMLField
+# from django.db import models
+# import datetime as dt
+# from django.contrib.auth.models import User
+# from tinymce.models import HTMLField
 
-# Create your models here.
-class Profile(models.Model):
-    prof_image = models.ImageField(upload_to='insta/', blank=True)
-    bio = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    def save_profile(self):
-        self.save() 
+# # Create your models here.
+# class Profile(models.Model):
+#     prof_image = models.ImageField(upload_to='insta/', blank=True)
+#     bio = models.TextField()
+#     pub_date = models.DateTimeField(auto_now_add=True)
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+#     def save_profile(self):
+#         self.save() 
 
-    @classmethod
-    def update_profile(cls,update):
-        pass
-    def delete_profile(self):
-        self.delete()  
-    @classmethod
-    def search_by_profile(cls,name):
-        profile = Profile.objects.filter(profile__insta_profile__icontains=search_term)
-        return profile   
+#     @classmethod
+#     def update_profile(cls,update):
+#         pass
+#     def delete_profile(self):
+#         self.delete()  
+#     @classmethod
+#     def search_by_profile(cls,name):
+#         profile = Profile.objects.filter(profile__insta_profile__icontains=search_term)
+#         return profile   
 
-class Image (models.Model):
-    image = models.ImageField(upload_to = 'insta/', default='No image')
-    image_name = models.CharField(max_length =500)
-    image_caption = HTMLField(blank=True)
-    # likes = models.ManyToManyField(likes)
-    # comment = models.ManyToManyField(comment)
-    profile = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now_add=True, null=True) 
-    class Meta:
-        ordering = ('-pub_date',)
-    def save_image(self):
-        self.save() 
-    @classmethod
-    def update_caption(cls,update):
-        pass
-    def delete_image(self):
-        self.delete()    
-    def display_images(cls):
-        images=cls.objects.all()
-        return images  
-class Like(models.Model):
-    name = models.CharField(max_length =30)
+# class Image (models.Model):
+#     image = models.ImageField(upload_to = 'insta/', default='No image')
+#     image_name = models.CharField(max_length =500)
+#     image_caption = HTMLField(blank=True)
+#     # likes = models.ManyToManyField(likes)
+#     # comment = models.ManyToManyField(comment)
+#     profile = models.ForeignKey(User, on_delete=models.CASCADE)
+#     pub_date = models.DateTimeField(auto_now_add=True, null=True) 
+#     class Meta:
+#         ordering = ('-pub_date',)
+#     def save_image(self):
+#         self.save() 
+#     @classmethod
+#     def update_caption(cls,update):
+#         pass
+#     def delete_image(self):
+#         self.delete()    
+#     def display_images(cls):
+#         images=cls.objects.all()
+#         return images  
+# class Like(models.Model):
+#     name = models.CharField(max_length =30)
 
-    def __str__(self):
-        return self.name
-class Comment(models.Model):
-    name = models.CharField(max_length =30)
+#     def __str__(self):
+#         return self.name
+# class Comment(models.Model):
+#     name = models.CharField(max_length =30)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
--- insta/views.py
+++ insta/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
-import datetime as dt
-from .models import Image
-from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render
+# from django.http  import HttpResponse
+# import datetime as dt
+# from .models import Image
+# from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-@login_required(login_url='/accounts/login/')
-def instagram(request):
-    image = Image.display_images
-    return render(request,'instagram.html',{"image":image})
+# # Create your views here.
+# @login_required(login_url='/accounts/login/')
+# def instagram(request):
+#     image = Image.display_images
+#     return render(request,'instagram.html',{"image":image})
 
-def image(request,image_id):
-    try:
-        image = Image.objects.get(id = image_id)
-    except DoesNotExist:
-        raise Http404()
-    return render(request,"all-insta/image.html", {"image":image})
+# def image(request,image_id):
+#     try:
+#         image = Image.objects.get(id = image_id)
+#     except DoesNotExist:
+#         raise Http404()
+#     return render(request,"all-insta/image.html", {"image":image})
