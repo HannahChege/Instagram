1fcb47d1a348c8e85331e04b40782ae01a45233f
HannahChege <chegehannah45@gmail.com>
2018-10-07 07:01:05 +0300

    Modified the readme

--- insta/models.py
+++ insta/models.py
@@ -2,54 +2,86 @@ from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # # Create your models here.
 class Profile(models.Model):
-    prof_image = models.ImageField(upload_to='insta/', blank=True)
     bio = models.TextField()
+    image = models.ImageField( blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    def __str__(self):
+        return self.user.username
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender,instance, **kwargs):
+        instance.profile.save()
+
     def save_profile(self):
-        self.save() 
+        self.save()  
+
+    def delete_profile(self):
+        self.delete()  
 
     @classmethod
     def update_profile(cls,update):
         pass
-    def delete_profile(self):
-        self.delete()  
+     
     @classmethod
     def search_by_profile(cls,name):
-        profile = Profile.objects.filter(profile__insta_profile__icontains=search_term)
-        return profile   
+        profile = Profile.objects.filter(user__username__icontains=name)
+        return profile 
+    @classmethod 
+    def get_by_id(cls,id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
 
 class Image (models.Model):
+    image_name = models.CharField(max_length =50)
     image = models.ImageField(upload_to = 'instas/', default='No image')
-    image_name = models.CharField(max_length =500)
     image_caption = HTMLField(blank=True)
-    # likes = models.ManyToManyField(likes)
-    # comment = models.ManyToManyField(comment)
     profile = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True, null=True) 
-    class Meta:
-        ordering = ('-pub_date',)
+    
     def save_image(self):
         self.save() 
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls,caption):
+        update_img = cls.objects.filter(id = id).update(caption = caption)
+        return images
+        pass      
+
     @classmethod
     def get_all_images(cls):
-        all_images = Image.objects.all()
-        return all_images 
+        images = cls.objects.all()
+        return images 
+    
     @classmethod
     def get_image_by_id(cls, id):
-        an_image = Image.objects.get(id=id)
-        return an_image       
+        image = cls.objects.filter(id=id).all()
+        return image       
     @classmethod
-    def update_caption(cls,update):
+    def get_profile_pic(cls,profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
         pass
-    def delete_image(self):
-        self.delete()    
+      
     def display_images(cls):
         images=cls.objects.all()
-        return images  
+        return images 
+     
 # class Like(models.Model):
 #     name = models.CharField(max_length =30)
 
