e3ed19726bfb337297d08abbc292e649d0091d2f
HannahChege <chegehannah45@gmail.com>
2018-10-18 10:03:16 +0300

    Changes made to the template

--- insta/tests.py
+++ insta/tests.py
@@ -4,33 +4,72 @@ from .models import Image,Profile
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
+class ProfileTestClass(TestCase):
+    """
+    Test profile class and its functions
+    """
     def setUp(self):
-        self.profile = Profile(bio='Life is too short')
-    def test_instances(self):
-        self.assertTrue(isinstance(self.profile,Profile))
+
+        self.user = User.objects.create(id =1,username='hannah')
+        self.profile = Profile(dp='hannah.jpg', bio='Life is too short', contact="0711139310",user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
     def test_save_method(self):
+        """
+        Function to test that profile is being saved
+        """
         self.profile.save_profile()
         profiles = Profile.objects.all()
-        self.assertTrue(len(profles) > 0)   
-    def test_delete_method(self):
-        self.profile.save_profile()
-        self.profile.delete_profile()
-        profile = Profile.objects.all()
-        self.assertTrue(len(profiel) == 0)
-    def test_search_by_profile(self):
-        profiles = Profile.search_profile('')
-        self.assertTrue(len(profiles)>0)  
+        self.assertTrue(len(profiles) > 0)
 
+    def test_delete_method(self):
+        """
+        Function to test that a profile can be deleted
+        """
+        self.profile.save_profile()
+        
+
+    def test_update_method(self):
+        """
+        Function to test that a profile's details can be updated
+        """
+        self.profile.save_profile()
+        new_profile = Profile.objects.filter(bio='LIfe is too short').update(bio='You only live once')
+
+    
+    def test_get_profile_by_id(self):
+        """
+        Function to test if you can get a profile by its id
+        """
+        self.profile.save_profile()
+        this_pro= self.profile.get_by_id(self.profile.user_id)
+        profile = Profile.objects.get(user_id=self.profile.user_id)
+        self.assertTrue(this_pro, profile)
+
+class CommentTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.coments = Comment(description = 'comment')
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.coments,Comment))
+    #testing for savinng method
+    def test_save_method(self):
+        self.coments.save_comment()
+        comments = Comment.objects.all()
+        self.assertTrue(len(comments) > 0)
+    #testng for deleting method
+    def test_delete_method(self):
+        self.coments.save_comment()
+        self.coments.delete_comment()
+        comments = Comment.objects.all()
+        self.assertTrue(len(comments) == 1 )         
 class ImageTestClass(TestCase):
     # Set up Method
     def setUp(self):
-        self.profile = Profile(bio='Life is too short',image='',user='')
-        self.proile.save_profile()
-
-
-        self.image = Image(name='image test', image='my test',caption='a caption test', profile='', profile_det=self.profile)
-        self.image.save_image()
-
+        self.image = Image(image = 'fashion')
     def test_instance(self):
         self.assertTrue(isinstance(self.image, Image))
 
@@ -43,7 +82,16 @@ class ImageTestClass(TestCase):
         self.image.save_image()
         images  = Image.objects.all()
         self.assertTrue(len(images)>0)
-
+    def test_delete_method(self):
+        self.image.save_image()
+        self.image.delete_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image) > 0)
+    def test_update_metod(self):
+        self.image.save_image()
+        self.image.update_caption()
+        image = Image.objects.all()
+        self.assertTrue(len(image) > 0)    
 
     def test_get_all_images(self):
         images = Image.get_all_images()
@@ -53,9 +101,5 @@ class ImageTestClass(TestCase):
         images= Image.get_image_by_id(self.image.id)
         self.assertTrue(len(images) == 1)
 
-    def test_get_profile_pic(self):
-        images = Image.get_profile_pic(profile.id)
-        self.assertTrue(len(images)>0)
-
-   
+    
 
